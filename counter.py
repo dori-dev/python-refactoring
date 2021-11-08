88130934ea9907d59e790417516f61f1c7b5bd78
mohammad <mr.dori.dev@gmail.com>
2021-11-08 16:12:33 +0330

    add comments

--- counter.py
+++ counter.py
@@ -1,14 +1,15 @@
-"""Count hashable objects
+"""Count any objects
 with collections.Counter
 """
 from collections import Counter
 
+# A good way to count values in any objects
 my_list = [4, 4, 4, 4, 8, 9, 7, 7, 7, 5, 5, 5, 5, 5, 5]
 counter = Counter(my_list)
 print(counter)
 print(counter[5])
 print(counter[10])
-
+# Get most of the values used in the object
 most_common = counter.most_common(2)
 print(most_common)
 most_common = counter.most_common(3)
