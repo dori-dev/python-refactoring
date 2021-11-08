23df7429c55b5a4692b1be3ea5ade5b7acf3c0a2
mohammad <mr.dori.dev@gmail.com>
2021-11-08 13:09:04 +0330

    added good way

--- define_dict_values.py
+++ define_dict_values.py
@@ -2,11 +2,21 @@
 with .get() and .setdefault()
 """
 
+
 my_dict = {
     "item": "footbal",
     "price": 10
 }
-
+# inappropriate code ✗
 # count = my_dict["count"]  # KeyError: 'count'
 # print(count)
 
+# appropriate code ✓
+# 1)
+count = my_dict.get("count", 1)
+print(count)
+
+# 2)
+count = my_dict.setdefault("count", 1)
+print(count)
+print(my_dict)
