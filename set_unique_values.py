310ec7f71af2e111b4065f54cf7c38ebfd35f3aa
mohammad <mr.dori.dev@gmail.com>
2021-11-08 12:51:57 +0330

    write bad and good codes

--- set_unique_values.py
+++ set_unique_values.py
@@ -2,3 +2,9 @@
 with sets()
 """
 
+my_list = [1, 3, 7, 7, 8, 5, 3, 2, 9, 7, 3]
+my_set = set(my_list)
+print(my_set)  # {1, 2, 3, 5, 7, 8, 9}
+
+unique_numbers = {1, 3, 7, 7, 8, 5, 3, 2, 9, 7, 3}
+print(unique_numbers)  # {1, 2, 3, 5, 7, 8, 9}
