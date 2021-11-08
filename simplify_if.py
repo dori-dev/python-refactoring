d84261e435666e30a7404e5d47160a2c5d2d356a
mohammad <mr.dori.dev@gmail.com>
2021-11-08 16:35:46 +0330

    added good and bad way

--- simplify_if.py
+++ simplify_if.py
@@ -1,2 +1,10 @@
 """Simplify if-statement for multiples checks
 with if x in [a,b,c]"""
+
+color = "red"
+if color == "red" or color == "blue" or color == "green":
+    print("its main color!")
+
+main_colors = ["red", "blue", "green"]
+if color in main_colors:
+    print("its main color!")
