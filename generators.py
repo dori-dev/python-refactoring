f203bc20ad54162bf8c8b2fa4672d1ef17c9dcc5
mohammad <mr.dori.dev@gmail.com>
2021-11-08 12:56:18 +0330

    add comments

--- generators.py
+++ generators.py
@@ -2,8 +2,10 @@
 with generators
 """
 
+# inappropriate code ✗
 my_list = [n**3 for n in range(10000)]
 print(sum(my_list))
 
+# appropriate code ✓
 my_generator = (n**3 for n in range(10000))
 print(sum(my_generator))
