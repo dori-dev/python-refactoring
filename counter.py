fd79d3f1b5c61b9fc6ff5de60fbe92cf04b77e44
mohammad <mr.dori.dev@gmail.com>
2021-11-08 16:09:02 +0330

    add codes to counter.py

--- counter.py
+++ counter.py
@@ -1,3 +1,15 @@
 """Count hashable objects
 with collections.Counter
 """
+from collections import Counter
+
+my_list = [4, 4, 4, 4, 8, 9, 7, 7, 7, 5, 5, 5, 5, 5, 5]
+counter = Counter(my_list)
+print(counter)
+print(counter[5])
+print(counter[10])
+
+most_common = counter.most_common(2)
+print(most_common)
+most_common = counter.most_common(3)
+print(most_common)
