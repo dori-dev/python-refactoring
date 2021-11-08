beabf3dbc7e39f26386a89db0acd6fd68425bbc9
mohammad <mr.dori.dev@gmail.com>
2021-11-08 12:43:21 +0330

    write codes

--- sort_coplex_iteration.py
+++ sort_coplex_iteration.py
@@ -1,3 +1,24 @@
 """Sort complex iterables
-with sorted()
+with build-in sorted() method
 """
+
+data = [3, 5, 1, 10, 9]
+sorted_data = sorted(data)
+print(sorted_data)
+
+data = (3, 5, 1, 10, 9)
+sorted_data = sorted(data, reverse=True)
+print(sorted_data)
+
+data = [
+    {
+        "name": "mohammad",
+        "family": "dori",
+        "age": 17,
+    },
+    {
+        "name": "ali",
+        "family": "dori",
+        "age": 20,
+    }
+]
