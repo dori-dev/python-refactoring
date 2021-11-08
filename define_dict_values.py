7091953bff91d1ec0d1818931878a83640d8fcb8
mohammad <mr.dori.dev@gmail.com>
2021-11-08 13:06:57 +0330

    added bad way

--- define_dict_values.py
+++ define_dict_values.py
@@ -1,3 +1,12 @@
 """Define default values in dictionaries
 with .get() and .setdefault()
 """
+
+my_dict = {
+    "item": "footbal",
+    "price": 10
+}
+
+# count = my_dict["count"]  # KeyError: 'count'
+# print(count)
+
