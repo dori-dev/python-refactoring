b8b552ccfe6b0a5c846afe87a7405fc5907e2cff
mohammad <mr.dori.dev@gmail.com>
2021-11-08 12:31:58 +0330

    add enumerate file

---
+++ enumerate_iterate.py
@@ -0,0 +1,20 @@
+"""Iterate with enumerate
+instead of range(len(x))
+"""
+
+
+# inappropriate code ✗
+data = [1, -5, 8, -3]
+for i in range(len(data)):
+    if data[i] < 0:
+        data[i] = 0
+
+print(data)  # [1, 0, 8, 0]
+
+
+# appropriate code ✓
+data = [1, -5, 8, -3]
+for index, number in enumerate(data):
+    if number < 0:
+        data[index] = 0
+print(data)  # [1, 0, 8, 0]
