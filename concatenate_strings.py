18a1ee9af215db1b62164262dedd5c34f574eae0
mohammad <mr.dori.dev@gmail.com>
2021-11-08 16:27:53 +0330

    add comments

--- concatenate_strings.py
+++ concatenate_strings.py
@@ -1,13 +1,14 @@
 """Concatenate string
 with .join()"""
 
+# inappropriate code ✗
 list_of_string = ["Hi", "Mohammad", "Dori"]
 my_string = ""
 for word in list_of_string:
     my_string += word + " "
 print(my_string)
 
-
+# appropriate code ✓
 list_of_string = ["Hi", "Mohammad", "Dori"]
 my_string = " ".join(list_of_string)
 print(my_string)
