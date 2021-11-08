df56232db17376bdbf8dc25689cec5d186f01006
mohammad <mr.dori.dev@gmail.com>
2021-11-08 12:58:37 +0330

    test the tow way

--- generators.py
+++ generators.py
@@ -1,11 +1,14 @@
 """Save memory
 with generators
 """
+import sys
 
 # inappropriate code ✗
 my_list = [n**3 for n in range(10000)]
 print(sum(my_list))
+print(sys.getsizeof(my_list), "bytes")  # 87616
 
 # appropriate code ✓
 my_generator = (n**3 for n in range(10000))
 print(sum(my_generator))
+print(sys.getsizeof(my_generator), "bytes")  # 112
