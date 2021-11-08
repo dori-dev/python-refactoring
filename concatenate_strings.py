af2d68d4d8be297cbc829426abab53baf4d59dac
mohammad <mr.dori.dev@gmail.com>
2021-11-08 16:27:35 +0330

    add good and bad way

--- concatenate_strings.py
+++ concatenate_strings.py
@@ -1,2 +1,17 @@
 """Concatenate string
 with .join()"""
+
+list_of_string = ["Hi", "Mohammad", "Dori"]
+my_string = ""
+for word in list_of_string:
+    my_string += word + " "
+print(my_string)
+
+
+list_of_string = ["Hi", "Mohammad", "Dori"]
+my_string = " ".join(list_of_string)
+print(my_string)
+
+names = ["Mohammad", "Salar", "Ali"]
+my_string = " and ".join(names)
+print(f"Hello {my_string}")
