21c391bbd92c52a858312898c5078060b5360919
mohammad <mr.dori.dev@gmail.com>
2021-11-08 12:34:32 +0330

    add good code

--- list_comprehension.py
+++ list_comprehension.py
@@ -4,6 +4,9 @@ instead of raw for loops
 
 squares = []
 
-for i in range(10):
-    squares.append(i * i)
-print(squares)
+for n in range(10):
+    squares.append(n * n)
+print(squares)  # [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+
+squares = [n * n for n in range(10)]
+print(squares)  # [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
