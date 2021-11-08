0a2f8f487600b3e2fd49f4adb5e8f04a9e2bbff9
mohammad <mr.dori.dev@gmail.com>
2021-11-08 16:17:57 +0330

    added comments

--- format_strings.py
+++ format_strings.py
@@ -1,9 +1,16 @@
 """Format Strings(python 3.6+)
 with f-Strings"""
 
+# inappropriate code ✗
+name = "Mohammad"
+my_string = "Hello " + name
+print(my_string)
+for number in range(10):
+    print(number, "squared is", number ** 2)
+
+# appropriate code ✓
 name = "Mohammad"
 my_string = f"Hello {name}"
 print(my_string)
-
 for number in range(10):
     print(f"{number} squared is {number**2}")
