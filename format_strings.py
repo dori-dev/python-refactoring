a8ecb748b6a0cdcc91670a23f3060699f6c4fffa
mohammad <mr.dori.dev@gmail.com>
2021-11-08 16:15:36 +0330

    added good way

--- format_strings.py
+++ format_strings.py
@@ -1,2 +1,9 @@
 """Format Strings(python 3.6+)
 with f-Strings"""
+
+name = "Mohammad"
+my_string = f"Hello {name}"
+print(my_string)
+
+for number in range(10):
+    print(f"{number} squared is {number**2}")
