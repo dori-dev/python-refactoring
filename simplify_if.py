b9b5c0fa15c6ac7516cf815218fa5c6aaa3ce558
mohammad <mr.dori.dev@gmail.com>
2021-11-08 16:36:37 +0330

    added comment to simplify_if .py

--- simplify_if.py
+++ simplify_if.py
@@ -1,10 +1,13 @@
 """Simplify if-statement for multiples checks
 with if x in [a,b,c]"""
 
+# inappropriate code ✗
 color = "red"
 if color == "red" or color == "blue" or color == "green":
     print("its main color!")
 
+# appropriate code ✓
+color = "red"
 main_colors = ["red", "blue", "green"]
 if color in main_colors:
     print("its main color!")
