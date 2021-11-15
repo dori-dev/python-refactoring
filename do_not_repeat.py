3383e90588e3ce5a776c6f1b14d5865ce236099a
mohammad <mr.dori.dev@gmail.com>
2021-11-15 15:58:34 +0330

    add comments

--- do_not_repeat.py
+++ do_not_repeat.py
@@ -3,6 +3,7 @@ Avoid duplicate codes"""
 
 NUMBER = 100
 
+# inappropriate code ✗
 if NUMBER < 50:
     result = NUMBER ** 3
     result = f"Result: {result}"
@@ -10,10 +11,9 @@ else:
     result = NUMBER ** 2
     result = f"Result: {result}"
 
-
+# appropriate code ✓
 if NUMBER < 50:
     result = NUMBER ** 3
 else:
     result = NUMBER ** 2
-
 label = f"Result: {result}"
