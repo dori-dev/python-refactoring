be092a563f616908605dda258921e553c3c649aa
mohammad <mr.dori.dev@gmail.com>
2021-11-15 16:04:31 +0330

    add  bad code

--- use_fewer_loops.py
+++ use_fewer_loops.py
@@ -1,2 +1,9 @@
 """Do not use too many loops
 """
+
+found = False
+conditions = [False, False, False, True, False, True, True]
+for condition in conditions:
+    if condition:
+        fount = True
+        break
