22bc1ac4e1f55f81983dede0686cd7231ee225d2
mohammad <mr.dori.dev@gmail.com>
2021-11-15 16:06:01 +0330

    add comments

--- use_fewer_loops.py
+++ use_fewer_loops.py
@@ -1,14 +1,14 @@
 """Do not use too many loops
 """
 
+# inappropriate code ✗
 found = False
 conditions = [False, False, False, True, False, True, True]
 for condition in conditions:
     if condition:
         fount = True
         break
-print(fount)
 
+# appropriate code ✓
 conditions = [False, False, False, True, False, True, True]
 found = any(conditions)
-print(found)
