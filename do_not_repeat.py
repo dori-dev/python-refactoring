ffcd7e4c4d9662272d74e2f4e206423a6981d4bf
mohammad <mr.dori.dev@gmail.com>
2021-11-15 15:56:59 +0330

    add codes

--- do_not_repeat.py
+++ do_not_repeat.py
@@ -1,2 +1,19 @@
 """Do not repeat and
 Avoid duplicate codes"""
+
+NUMBER = 100
+
+if NUMBER < 50:
+    result = NUMBER ** 3
+    result = f"Result: {result}"
+else:
+    result = NUMBER ** 2
+    result = f"Result: {result}"
+
+
+if NUMBER < 50:
+    result = NUMBER ** 3
+else:
+    result = NUMBER ** 2
+
+label = f"Result: {result}"
