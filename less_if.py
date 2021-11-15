b7d83718e0b5cc56d6a6d337f09e08dd1aca509a
mohammad <mr.dori.dev@gmail.com>
2021-11-15 15:51:24 +0330

    add codes

--- less_if.py
+++ less_if.py
@@ -1,2 +1,12 @@
 """Use less "if"
 """
+
+CONDITION_a = True
+CONDITION_b = True
+
+if CONDITION_a:
+    if CONDITION_b:
+        print('mohammad dori')
+
+if CONDITION_a and CONDITION_b:
+    print('mohammad dori')
